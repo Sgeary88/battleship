28b293de34ff3b9947cef268b9c79b262a172961
Sgeary88 <97266345+Sgeary88@users.noreply.github.com>
2023-09-26 19:06:00 -0700

    refactored part 1

--- part1.js
+++ part1.js
@@ -4,6 +4,20 @@ let board = [];
 let boardSize = 3;
 let remainingShips = 2;
 
+function createBoard() {
+	for (let i = 0; i < boardSize; i++) {
+		let row = [];
+		for (let j = 0; j < boardSize; j++) {
+			row.push(' ');
+		}
+		board.push(row);
+	}
+}
+
+function getRandomValue() {
+	return Math.floor(Math.random() * boardSize);
+}
+
 
 function countHits() {
 	let hits = 0;
@@ -17,61 +31,7 @@ function countHits() {
 	return hits;
 }
 
-
-
-function getGuesses() {
-	let input = rs.question('Enter strike location (A1, A2, A3, B1, B2, B3, C1, C2, C3: ');
-	let inputX = translateToNumber(input[0]);
-	let inputY = Number(input[1]) - 1;
-	
-	// for invalid inputs
-	if (inputX === -1 || isNaN(inputY) || inputY < 0 || inputY >= boardSize) {
-		console.log('Invalid input. Please try again.');
-	}
-
-	if (board[inputX][inputY] === 'S') {
-		board[inputX][inputY] = 'H';
-		console.log(`Hit! You have sunk a battleship! ${remainingShips - 1} ships remaining`);
-		remainingShips--;
-	}
-	else if (board[inputX][inputY] === 'H' || board[inputX][inputY] === 'X') {
-		console.log('You have already picked this location. Miss!');
-	}
-	else {
-		board[inputX][inputY] = 'X';
-		console.log('Miss!');
-	}
-
-	hits = countHits();
-	if (hits === 2) {
-		console.log('You have sunk all the Battleships!')
-		let answer = rs.keyInYN('Play Again?')
-		if (answer) {
-			startGame();
-		} else {
-			return console.log('Bye Loser!');
-		}
-	} else {
-		console.log(board)
-			getGuesses();
-		}
-	}
-
-
-function translateToNumber(letter) {
-	letter = letter.toUpperCase();
-
-	const alphabet = {
-		'A': 0,
-		'B': 1,
-		'C': 2,
-	}
-
-	return Object.keys(alphabet).includes(letter) ? alphabet[letter] : -1;
-
-}
-
-function randomShipPlacement () {
+function randomShipPlacement() {
 	let x;
 	let y;
 
@@ -88,19 +48,71 @@ function randomShipPlacement () {
 	board[x][y] = 'S';
 }
 
-function createBoard () {
-	for (let i = 0; i < boardSize; i++) {
-		let row = [];
-		for (let j = 0; j < boardSize; j++) {
-			row.push(' ');
-		}
-		board.push(row);
+function translateToNumber(letter) {
+	letter = letter.toUpperCase();
+
+	const alphabet = {
+		'A': 0,
+		'B': 1,
+		'C': 2,
+	}
+
+	return Object.keys(alphabet).includes(letter) ? alphabet[letter] : -1;
+
+}
+
+function isInputValid(inputX, inputY) {
+	return inputX >= 0 && inputX < boardSize && inputY >= 0 && inputY < boardSize && !isNaN(inputY);
+}
+
+
+function handleHit(inputX, inputY) {
+	console.log('Hit');
+	board[inputX][inputY] = 'O';
+};
+
+
+function handleMiss(inputX, inputY) {
+	if (board[inputX][inputY] === 'O' || board[inputX][inputY] === 'X') {
+		console.log('You have already chosen this location. Miss!');
+	} else {
+		board[inputX][inputY] = 'X';
+		console.log('Miss!');
 	}
 }
 
-function getRandomValue () {
-	return Math.floor(Math.random() * boardSize);
+
+function getGuesses() {
+	let input = rs.question('Enter strike location (A1, A2, A3, B1, B2, B3, C1, C2, C3: ');
+	let inputX = translateToNumber(input[0]);
+	let inputY = Number(input[1]) - 1;
+
+	// for invalid inputs
+	if (!isInputValid(inputX, inputY)) {
+		console.log('Invalid input. Please try again.');
+	} else {
+		if (board[inputX][inputY] === 'S') {
+			handleHit(inputX, inputY);
+			remainingShips--;
+		} else {
+			handleMiss(inputX, inputY);
+		}
+	}
+
+	if (remainingShips !== 0) {
+		console.log(board);
+		getGuesses();
+	} else {
+		console.log('You have sunk all the Battleships!');
+		let answer = rs.keyInYN('Play Again?');
+		if (answer) {
+			startGame();
+		} else {
+			console.log('Bye Loser!');
+		}
+	}
 }
+
 
 
 function startGame() {
